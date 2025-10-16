456f771c32eb69452f01492d287d1537bd69ec6c
fonte15 <franciscofonte0@gmail.com>
2025-10-16 16:14:46 +0100

    llread

--- lab1-code-25-26(1)/src/link_layer.c
+++ lab1-code-25-26(1)/src/link_layer.c
@@ -2,7 +2,7 @@
 
 #include "link_layer.h"
 #include "serial_port.h"
-#include "lab1/read.c"
+#include "../lab1/read.c"
 
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
@@ -33,37 +33,21 @@ int llwrite(const unsigned char *buf, int bufSize)
 int llread(unsigned char *packet)
 {
     
-
-    // Open serial port device for reading and writing, and not as controlling tty
-    // because we don't want to get killed if linenoise sends CTRL-C.
-    //
-    // NOTE: See the i// code blockmplementation of the serial port library in "serial_port/".
-    
-
-    // Read from serial port until the 'z' char is received.
-
-    // NOTE: This while() cycle is a simple example showing how to read from the serial port.
-    // It must be changed in order to respect the specifications of the protocol indicated in the Lab guide.
-
-    // TODO: Save the received bytes in a buffer array and print it at the end of the program.
     int nBytesBuf = 0;
 
     State state = Start;
-
+    unsigned char ctrl = 0;
+    unsigned char result[BUF_SIZE]; 
+    int index = 0;
+    STOP = FALSE;
 
 
     while (STOP == FALSE)
     {
-        // Read one byte from serial port.
-        // NOTE: You must check how many bytes were actually read by reading the return value.
-        // In this example, we assume that the byte is always read, which may not be true.
+
         unsigned char byte;
         int bytes = readByteSerialPort(&byte);
         nBytesBuf += bytes;
-
-
-
-
         
         switch (state) {
             case Start:
@@ -88,98 +72,103 @@ int llread(unsigned char *packet)
                 printf("a\n");
                 if(byte == FLAG) state = Flag_RCV;
 
-                else if(byte == SET) state = C_RCV;
-
-                else if(byte == 0xAA) state = C_INF_RCV;
-
-                else state = Start;
-                
+                else {
+                    ctrl = byte;
+                    state = C_RCV;
+                }
                 break;
 
             case C_RCV:
                 printf("c\n");
                 if(byte == FLAG) state = Flag_RCV;
 
-                else if(byte == 0) state = BCC_RCV;
+                else if (byte == (add1 ^ ctrl)) state = BCC_RCV;
 
                 else state = Start;
                 
                 break;
 
-            case C_INF_RCV:
-                printf("c_inf\n");
-                if(byte == FLAG) state = Flag_RCV;
-                
-                else if (byte == (add1 ^ byte)) state = BCC_INF_RCV;
-                
-                else state = Start;
-                
-                break;
+            
 
             case BCC_RCV:
 
                 printf("bcc\n");
-                if(byte == FLAG){
-                    state = Stop;
-                    STOP = TRUE;
-                } 
+                
+                if(ctrl == SET){
+                    
+                    if(byte == FLAG){
+                        
+                        state = Stop;
+                        STOP = TRUE;
 
-                else state = Start;
-                break;
+                        unsigned char buf[5] = {0}; 
 
-            case BCC_INF_RCV:
-                printf("bcc_inf\n");
-                state = DATA_RCV;
-                break;
+                        buf[0] = FLAG;
+                        buf[1] = add1;
+                        buf[2] = UA;
+                        buf[3] = add1 ^ UA;
+                        buf[4] = FLAG;
+
+                        writeBytesSerialPort(buf, 5);
+                    } 
+
+                    else state = Start;
+                }
+                else if(ctrl == 0xAA){
+                    if(byte == FLAG){
+                        state = Stop;
+                        STOP = TRUE;
+                        
+                    } 
+                    else {
+                        result[index++] = byte;
+                        state = DATA_RCV;
+                    }
+
+                }
+
 
             case DATA_RCV:
                 printf("data\n");
                 
+                if(byte == FLAG){
+                    state = Stop;
+                    STOP = TRUE;
+                        
+                } 
+                else {
+                    result[index++] = byte;
+                }
                 break;
             
-                case Stop:
+            case Stop:
                 printf("stop\n");
                 STOP = TRUE;
                 break;
+            
             default:
                 break;
         }
+
+        
         }
         
         
 
-
-
-    printf("SENDING NOW");
-    unsigned char buf[BUF_SIZE] = {0};
-
-    buf[0] = FLAG;
-    buf[1] = add1;
-    buf[2] = UA;
-    buf[3] = add1 ^ UA;
-    buf[4] = FLAG;
-
-    sleep(1);
-    int bytes = writeBytesSerialPort(buf, 5);
-    int i = 0;
-    while(i<5){
-        printf("var = 0x%02X\n", buf[i]);
-        i++;
+    for (int i = 0; i < index; i++) {
+        packet[i] = result[i];
     }
 
+    
 
-    printf("Total bytes received: %d\n", nBytesBuf);
-
-    // Close serial port
-    if (closeSerialPort() < 0)
-    {
-        perror("closeSerialPort");
-        exit(-1);
+    printf("Received packet: ");
+    for (int i = 0; i < index; i++) {
+        printf("%02X ", packet[i]);
     }
+    printf("\n");
 
-    printf("Serial port %s closed\n", serialPort);
 
-    return 0;
+    return index;
 }
 
 ////////////////////////////////////////////////
@@ -191,3 +180,5 @@ int llclose()
 
     return 0;
 }
+
+
